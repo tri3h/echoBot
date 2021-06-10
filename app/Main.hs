5628143a72e00d6bb60cc2f2f7c2798806e3087f
tri3h <tri3h@ya.ru>
2021-06-10 19:13:42 +0500

    Add getting message offset

--- app/Main.hs
+++ app/Main.hs
@@ -4,10 +4,12 @@ module Main where
 
 import Network.HTTP.Simple
 import Data.ByteString
+import Data.ByteString.Char8 as Char8
 import Data.Aeson
 import Data.Aeson.Types
 
-data Message = Message { chatID :: Integer, 
+data Message = Message { updateID :: Integer,
+                         chatID :: Integer, 
                          fromChatID :: Integer,
                          messageID :: Integer,
                          text :: Maybe String } deriving Show
@@ -15,6 +17,7 @@ data Message = Message { chatID :: Integer,
 instance FromJSON Message where
     parseJSON = withObject "Message" $ \obj -> do
         [result] <- obj .: "result"
+        updateID <- result .: "update_id"
         message <- result .: "message"
         messageID <- message .: "message_id"
         from <- message .: "from"
@@ -22,7 +25,8 @@ instance FromJSON Message where
         chat <- message .: "chat"
         chatID <- chat .: "id"
         text <- message .:? "text"
-        return Message {chatID = chatID, 
+        return Message {updateID = updateID,
+                        chatID = chatID, 
                         fromChatID = fromChatID,
                         messageID = messageID,
                         text = text}
@@ -31,9 +35,9 @@ instance FromJSON Message where
 token :: ByteString
  
 
-getUpdates :: IO Value
-getUpdates = do 
-            let path = "/bot" <> token <> "/getUpdates?limit=1"
+getUpdates :: ByteString -> IO Value
+getUpdates offset = do 
+            let path = "/bot" <> token <> "/getUpdates?limit=1" <> offset
             let request = setRequestPath path 
                             $ setRequestHost "api.telegram.org" 
                             $ setRequestPort 443
@@ -41,8 +45,12 @@ getUpdates = do
             response <- httpJSON request
             return $ getResponseBody response
 
+getOffset :: Maybe Message -> ByteString
+getOffset Nothing = ""
+getOffset (Just mes) = Char8.pack . show $ updateID mes + 1
+
 main :: IO ()
 main = do 
-    r <- getUpdates
+    r <- getUpdates $ getOffset Nothing
     let r' = parseMaybe parseJSON r :: Maybe Message
     print r'
