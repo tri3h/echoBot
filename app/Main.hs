d82a8dbded7a1965e2ad3972548103c0d5fe237c
tri3h <tri3h@ya.ru>
2021-06-11 08:26:49 +0500

    Change getting update

--- app/Main.hs
+++ app/Main.hs
@@ -25,32 +25,36 @@ instance FromJSON Message where
         chat <- message .: "chat"
         chatID <- chat .: "id"
         text <- message .:? "text"
-        return Message {updateID = updateID,
-                        chatID = chatID, 
-                        fromChatID = fromChatID,
-                        messageID = messageID,
-                        text = text}
+        return Message { updateID = updateID,
+                         chatID = chatID, 
+                         fromChatID = fromChatID,
+                         messageID = messageID,
+                         text = text }
 
 -- ??? take it from configs
 token :: ByteString
  
 
-getUpdates :: ByteString -> IO Value
-getUpdates offset = do 
-            let path = "/bot" <> token <> "/getUpdates?limit=1" <> offset
-            let request = setRequestPath path 
+sendRequest :: ByteString -> Query -> IO Value
+sendRequest path param = do 
+            let fullPath = "/bot" <> token <> path
+            let request = setRequestQueryString param
+                            $ setRequestPath fullPath 
                             $ setRequestHost "api.telegram.org" 
                             $ setRequestPort 443
                             $ setRequestSecure True defaultRequest 
             response <- httpJSON request
             return $ getResponseBody response
 
-getOffset :: Maybe Message -> ByteString
-getOffset Nothing = ""
-getOffset (Just mes) = Char8.pack . show $ updateID mes + 1
+getUpdate :: Maybe Message -> IO Value 
+getUpdate mes = sendRequest "/getUpdates" param 
+    where param = [("limit", Just "1"), ("offset", getOffset mes)] 
+          getOffset :: Maybe Message -> Maybe ByteString
+          getOffset Nothing = Nothing
+          getOffset (Just mes) = Just . Char8.pack . show $ updateID mes + 1
 
 main :: IO ()
 main = do 
-    r <- getUpdates $ getOffset Nothing
+    r <- getUpdate Nothing
     let r' = parseMaybe parseJSON r :: Maybe Message
     print r'
