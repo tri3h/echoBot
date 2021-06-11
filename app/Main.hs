58ae713e0d4859679b043e04b458f2bde143513a
tri3h <tri3h@ya.ru>
2021-06-11 11:02:48 +0500

    Add sending and repeating messages

--- app/Main.hs
+++ app/Main.hs
@@ -48,13 +48,31 @@ sendRequest path param = do
 
 getUpdate :: Maybe Message -> IO Value 
 getUpdate mes = sendRequest "/getUpdates" param 
-    where param = [("limit", Just "1"), ("offset", getOffset mes)] 
+    where param = [("limit", Just "1"), 
+                   ("offset", getOffset mes)] 
           getOffset :: Maybe Message -> Maybe ByteString
           getOffset Nothing = Nothing
-          getOffset (Just mes) = Just . Char8.pack . show $ updateID mes + 1
+          getOffset (Just mes) = Just . toByteString $ updateID mes + 1
+
+sendMessage :: ByteString -> ByteString -> IO Value
+sendMessage chatID text = sendRequest "/sendMessage" param
+    where param = [("chat_id", Just chatID), 
+                   ("text", Just text)]
+
+repeatMessage :: Message -> IO Value
+repeatMessage mes = sendRequest "/copyMessage"
+                    $ fmap (\(a, b) -> (a, Just $ toByteString b)) param
+    where param = [("chat_id", chatID mes), 
+                   ("from_chat_id", fromChatID mes),
+                   ("message_id", messageID mes)]
+
+toByteString :: Show a => a -> ByteString 
+toByteString x = Char8.pack $ show x
 
 main :: IO ()
 main = do 
     r <- getUpdate Nothing
     let r' = parseMaybe parseJSON r :: Maybe Message
+    case r' of
+        Just mes -> repeatMessage mes
     print r'
