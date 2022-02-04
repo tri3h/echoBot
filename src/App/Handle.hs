c07870c3f49399011f9a23a13848a4f96ee5831e
tri3h <tri3h@ya.ru>
2022-02-04 18:52:50 +0500

    Change handle (add getUpdate, change functions return type)

--- src/App/Handle.hs
+++ src/App/Handle.hs
@@ -2,48 +2,55 @@
 
 module App.Handle where
 
+{-
 data Config = Config {
 path :: FilePath,
 repeatText :: String,
 helpText :: String
 }
+-}
 
 data Handle m a b = Handle {
-hConfig :: Config,
+--hConfig :: Config,
 getMessage :: a -> m b,
-makeHelpReq :: b -> a,
-makeRepeatNumReq :: b -> a,
-makeRepeatReq :: b -> a,
-makeMessageReq :: Maybe b -> a,
-changeRepeatNum :: b -> m (),
-getRepeatNum :: Maybe b -> Int
+getText :: b -> String,
+makeHelpReq :: b -> m a,
+makeRepeatNumReq :: b -> m a,
+makeRepeatReq :: b -> m a,
+makeMessageReq :: Maybe b -> m a,
+setRepeatNum :: b -> m b,
+getRepeatNum :: Maybe b -> m Int
 }
 
-chooseAnswer :: Monad m => Handle m a b -> b -> String -> m ()
-chooseAnswer handle mes s = case s of
+getUpdate :: Monad m => Handle m a b -> Maybe b -> m b
+getUpdate handle mes = do
+    req <- makeMessageReq handle mes
+    newMes <- getMessage handle req
+    chooseAnswer handle newMes
+
+chooseAnswer :: Monad m => Handle m a b -> b -> m b
+chooseAnswer handle mes = case getText handle mes of
     "/help" -> sendHelp handle mes
-    "/repeat" -> setNewRepeatNum handle mes
+    "/repeat" -> changeRepeatNum handle mes
     _ -> repeatMessage handle mes 
 
-setNewRepeatNum :: Monad m => Handle m a b -> b -> m ()
-setNewRepeatNum handle mes = do
-    let req = makeRepeatNumReq handle mes
-    numMes <- getMessage handle req
-    changeRepeatNum handle numMes
-
-sendHelp :: Monad m => Handle m a b -> b -> m ()
+sendHelp :: Monad m => Handle m a b -> b -> m b
 sendHelp handle mes = do
-    let req = makeHelpReq handle mes
+    req <- makeHelpReq handle mes
     getMessage handle req
-    return ()
 
-repeatMessage :: Monad m => Handle m a b -> b -> m ()
+changeRepeatNum :: Monad m => Handle m a b -> b -> m b
+changeRepeatNum handle mes = do
+    req <- makeRepeatNumReq handle mes
+    numMes <- getMessage handle req
+    setRepeatNum handle numMes
+
+repeatMessage :: Monad m => Handle m a b -> b -> m b
 repeatMessage handle mes = do
-    let num = getRepeatNum handle (Just mes)
-        req = makeRepeatReq handle mes
+    num <- getRepeatNum handle (Just mes)
+    req <- makeRepeatReq handle mes
     send handle req num
-    where send :: Monad m => Handle m a b -> a -> Int -> m ()
-          send _ _ 0 = return ()
+    where send handle req 1 = getMessage handle req
           send handle req n = do
               getMessage handle req
               send handle req (n-1)
