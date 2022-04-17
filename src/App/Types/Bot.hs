1be2d725fa6630ec848430fb87986311dcf3ca55
tri3h <tri3h@ya.ru>
2022-04-17 20:12:46 +0500

    Add error handling when config parameter is invalid #9

--- src/App/Types/Bot.hs
+++ src/App/Types/Bot.hs
@@ -1,5 +1,8 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module App.Types.Bot where
 
+import App.Handlers.Logger (Verbosity (Info))
 import qualified Data.ByteString as BS
 
 newtype UserID = UserID Integer deriving (Eq, Ord)
@@ -15,3 +18,15 @@ newtype Token = Token BS.ByteString
 newtype HelpText = HelpText BS.ByteString
 
 newtype RepeatText = RepeatText BS.ByteString
+
+defaultLogVerbosity :: Verbosity
+defaultLogVerbosity = Info
+
+defaultHelpText :: BS.ByteString
+defaultHelpText = "The most helpful text"
+
+defaultRepeatNum :: Integer
+defaultRepeatNum = 1
+
+defaultRepeatText :: BS.ByteString
+defaultRepeatText = "Echo sounds : "
